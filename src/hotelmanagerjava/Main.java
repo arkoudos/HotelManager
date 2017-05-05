3b538d16e4130ecf12aebc82b905ac46a1e4f729
stranger129 <mike.petrou9558@gmail.com>
2017-05-05 16:12:19 +0300

    Adding some input stuff Version 2

--- src/hotelmanagerjava/Main.java
+++ src/hotelmanagerjava/Main.java
@@ -9,24 +9,46 @@ public class Main
 {
     public static void main(String args[]) throws ParseException
     {
-        final JFileChooser fc = new JFileChooser();
+        String userDirLocation = System.getProperty("user.dir");
+        //JFileChooser fs = new JFileChooser();
+        //File f = new File(userDirLocation);
+        //fs.setCurrentDirectory(f);
+        //fs.showOpenDialog(null);
         
         String filePath = null;
         
-        File searchFile = new File("data2.csv");
+        File searchFile = new File("dat6a.csv");
+        
         if(searchFile.exists() && !searchFile.isDirectory())
-        {
-            filePath = "data.csv";
-        }else{
-            filePath = JOptionPane.showInternalInputDialog(null, "Enter the name of the file:", "File not found!",JOptionPane.OPTIONS_PROPERTY );
-            //filePath = JOptionPane.showInputDialog(null,"Enter the name of the file:", "File not found!", JOptionPane.QUESTION_MESSAGE);
+       {
+         filePath = "data.csv";
+       }else{
+            JFileChooser fs = new JFileChooser();
+            File f = new File(userDirLocation);
+            fs.setCurrentDirectory(f);
+            fs.showOpenDialog(null);
+            filePath = userDirLocation;
+         //  filePath = JOptionPane.showInternalInputDialog(null, "Enter the name of the file:", "File not found!",JOptionPane.OPTIONS_PROPERTY );
+          // filePath = JOptionPane.showInputDialog(null,"Enter the name of the file:", "File not found!", JOptionPane.QUESTION_MESSAGE);
         }
+//        final JFileChooser fc = new JFileChooser();
+//        
+//        String filePath = null;
+//        
+//        File searchFile = new File("data2.csv");
+//        if(searchFile.exists() && !searchFile.isDirectory())
+//        {
+//            filePath = "data.csv";
+//        }else{
+//            filePath = JOptionPane.showInternalInputDialog(null, "Enter the name of the file:", "File not found!",JOptionPane.OPTIONS_PROPERTY );
+//            //filePath = JOptionPane.showInputDialog(null,"Enter the name of the file:", "File not found!", JOptionPane.QUESTION_MESSAGE);
+//        }
         
         System.out.println(filePath);
         //FileManager test = new FileManager();
         //test.readFile();
      //   test.printArr();
         //Menu.runMain();
-        MainMenu.CreateMenu();
+       // MainMenu.CreateMenu();
     }
 }
